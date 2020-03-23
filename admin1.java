66854a327fef9bc750ae430ddac5d3128bc55c12
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-03-23 16:20:35 -0700

    Add files via upload

---
+++ admin1.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+public class admin1 {
+	public class Admin extends Stack
+	{
+		public void show()
+		{
+			System.out.println("\n\n---ADMIN---\nName:omar garmela\nID:LMS101");
+		}
+
+
+
